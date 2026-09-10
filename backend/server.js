55edc3a387466e3bee8a1922aeaa23fb0ca70473
nithishofficial32 <nithishofficial32@gmail.com>
2026-09-10 18:54:57 +0530

    Update backend port to use process.env.PORT

--- backend/server.js
+++ backend/server.js
@@ -97,3 +97,8 @@ app.use(express.static(path.join(__dirname, '../frontend/dist')));
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../frontend/dist', 'index.html'));
 });
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
